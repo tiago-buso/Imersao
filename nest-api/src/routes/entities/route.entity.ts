1cfa15fbff42853afd6d82a8e33be4c06f239e8e
Tiago Giannoni Buso <tiagogb_2@hotmail.com>
2022-06-22 19:19:07 -0300

    - atualizando o aplicativo

--- nest-api/src/routes/entities/route.entity.ts
+++ nest-api/src/routes/entities/route.entity.ts
@@ -1,1 +1,28 @@
-export class Route {}
+import { Schema, raw, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type RouteDocument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPostion: { lat: number; lng: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPostion: { lat: number; lng: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -1,11 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  OnModuleInit,
+} from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
 
 @Controller('routes')
-export class RoutesController {
-  constructor(private readonly routesService: RoutesService) {}
+export class RoutesController implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
@@ -22,6 +40,10 @@ export class RoutesController {
     return this.routesService.findOne(+id);
   }
 
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaClient.connect();
+  }
+
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
     return this.routesService.update(+id, updateRouteDto);
@@ -30,5 +52,18 @@ export class RoutesController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.routesService.remove(+id);
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    this.kafkaProducer.send({
+      topic: 'route.new-direction',
+      messages: [
+        {
+          key: 'route.new-direction',
+          value: JSON.stringify({ routeId: id, clientId: '' }),
+        },
+      ],
+    });
   }
 }
