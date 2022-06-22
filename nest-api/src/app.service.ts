b9db658c2c64c87ba3bb2fe0cb14a12522207292
Tiago Giannoni Buso <tiagogb_2@hotmail.com>
2022-06-22 16:37:36 -0300

    - atualizando os controllers e serviços de rota.

--- nest-api/src/app.service.ts
+++ nest-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World 4as89d4a987sd98as!';
   }
 }
